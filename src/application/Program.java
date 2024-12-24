aacca5c994d70f535b6fcfac01e577ffd9fe288a
Thiago Novaes <novaesthiago27@gmail.com>
2024-12-24 16:34:36 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,8 +34,13 @@ public class Program {
 		System.out.println("\n === TEST 4: seller insert === "); 
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department); 
 		sellerDao.insert(newSeller); 
-		System.out.println("Inserted! New Id = " + newSeller.getId()); 
+		System.out.println("Inserted! New Id = " + newSeller.getId());  
 		
+		System.out.println("\n === TEST 5: seller update === "); 
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller); 
+		System.out.println("Update Completed!");
 	}
 
 }
